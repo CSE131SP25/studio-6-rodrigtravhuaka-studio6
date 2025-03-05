88bdd59cc9b49f7d1ea1fb85c840778a29d59336
crusa <crusa@Emilee_Grace.dhcp.wustl.edu>
2025-03-05 14:18:10 -0600

    why?????

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -11,11 +11,14 @@ public class RecursiveMethods {
 	 * @return the sum of the first n terms of the geometric series (1/2 + 1/4 + 1/8
 	 *         ...)
 	 */
-	public static double geometricSum(int n) {
-		
+	public static double geometricSum(int n) { 
+		if ( n == 0 ) { 
+			return 0 ;
+		} else {
+		double value = Math.pow(0.5, n);  
 			// FIXME compute the geometric sum for the first n terms recursively
-			return 0;
-		
+			return geometricSum(n-1) + value;
+		}
 	}
 
 	
@@ -29,11 +32,19 @@ public class RecursiveMethods {
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
+		if (radius < radiusMinimumDrawingThreshold) {
+			return; 
+		} else {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(radius + xCenter, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, radius + yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter - radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter - radius, radius/3.0, radiusMinimumDrawingThreshold);
+		}
 		
-		// FIXME complete the recursive drawing
-	}
+		
 	
-
+	}
 	/**
 	 * This method uses recursion to create a reverse of the given array
 	 * 
@@ -41,12 +52,23 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
+		int[] reversed = new int [array.length];
+		int index = array.length - 1; 
+		helper(reversed, array, index);
 			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
+			return reversed;
 		
 	}
-
+	public static int[] helper(int[] reversed, int[] array, int index) {
+		if (index >= 0) {
+			reversed[reversed.length - index - 1] = array[index];
+			helper(reversed, array, index-1);
+		}
+		return reversed; }
+		
+		
+		
+	
 	/**
 	 * This method uses recursion to compute the greatest common divisor
 	 * for the two input values
